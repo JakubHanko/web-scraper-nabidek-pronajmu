a2cc5c9f41156983409f714961e6c5059913790d
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2025-03-16 16:51:05 +0100

    Remove unwanted scrapers

--- src/scrapers_manager.py
+++ src/scrapers_manager.py
@@ -6,27 +6,19 @@ from disposition import Disposition
 from scrapers.rental_offer import RentalOffer
 from scrapers.scraper_base import ScraperBase
 from scrapers.scraper_bravis import ScraperBravis
-from scrapers.scraper_euro_bydleni import ScraperEuroBydleni
 from scrapers.scraper_idnes_reality import ScraperIdnesReality
-from scrapers.scraper_realcity import ScraperRealcity
-from scrapers.scraper_realingo import ScraperRealingo
 from scrapers.scraper_remax import ScraperRemax
 from scrapers.scraper_sreality import ScraperSreality
 from scrapers.scraper_ulov_domov import ScraperUlovDomov
-from scrapers.scraper_bezrealitky import ScraperBezrealitky
 
 
 def create_scrapers(dispositions: Disposition) -> list[ScraperBase]:
     return [
         ScraperBravis(dispositions),
-        ScraperEuroBydleni(dispositions),
         ScraperIdnesReality(dispositions),
-        ScraperRealcity(dispositions),
-        ScraperRealingo(dispositions),
         ScraperRemax(dispositions),
         ScraperSreality(dispositions),
         ScraperUlovDomov(dispositions),
-        ScraperBezrealitky(dispositions),
     ]
 
 
